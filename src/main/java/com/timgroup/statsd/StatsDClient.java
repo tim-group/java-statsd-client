8eb0cff5bd6cdfabfcdf2896d61ca6f4404d89f3
Tom Denley <tom.denley@timgroup.com>
2014-07-25 17:06:36 +0100

    tomd: update JavaDoc for set support

--- src/main/java/com/timgroup/statsd/StatsDClient.java
+++ src/main/java/com/timgroup/statsd/StatsDClient.java
@@ -3,15 +3,16 @@ package com.timgroup.statsd;
 /**
  * Describes a client connection to a StatsD server, which may be used to post metrics
  * in the form of counters, timers, and gauges.
- * 
- * <p>Three key methods are provided for the submission of data-points for the application under
+ *
+ * <p>Four key methods are provided for the submission of data-points for the application under
  * scrutiny:
  * <ul>
  *   <li>{@link #incrementCounter} - adds one to the value of the specified named counter</li>
  *   <li>{@link #recordGaugeValue} - records the latest fixed value for the specified named gauge</li>
  *   <li>{@link #recordExecutionTime} - records an execution time in milliseconds for the specified named operation</li>
+ *   <li>{@link #recordSetEvent} - records an occurrence of the specified named event</li>
  * </ul>
- * 
+ *
  * @author Tom Denley
  *
  */
